6c07e7ee4171e12f55a57ed88229cc236788f003
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-13 06:37:54 +0530

    Ref Attribute

--- src/main/java/com/dilusha/Alien.java
+++ src/main/java/com/dilusha/Alien.java
@@ -2,11 +2,32 @@ package com.dilusha;
 
 public class Alien {
 
+    private int age;
+    private Laptop lap;
+
     public Alien() {
         System.out.println("Object Created");
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
--- src/main/java/com/dilusha/App.java
+++ src/main/java/com/dilusha/App.java
@@ -8,10 +8,14 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1 = (Alien) context.getBean("alien");
+        Alien obj1 = (Alien) context.getBean("alien1");
+        //obj1.setAge(21);
+        System.out.println(obj1.getAge());
         obj1.code();
 
-        Alien obj2 = (Alien) context.getBean("alien");
-        obj2.code();
+
+//        Alien obj2 = (Alien) context.getBean("alien");
+//        System.out.println(obj2.age);
+//        //obj2.code();
     }
 }
