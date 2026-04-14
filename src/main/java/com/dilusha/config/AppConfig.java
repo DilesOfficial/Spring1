b7be2457effee4ecd9e64279330f953441b581be
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-14 08:58:32 +0530

    Primary And Qualifier

--- src/main/java/com/dilusha/config/AppConfig.java
+++ src/main/java/com/dilusha/config/AppConfig.java
@@ -3,9 +3,12 @@ package com.dilusha.config;
 import com.dilusha.Alien;
 import com.dilusha.Computer;
 import com.dilusha.Desktop;
+import com.dilusha.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.annotation.AliasFor;
 
@@ -13,7 +16,7 @@ import org.springframework.core.annotation.AliasFor;
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Autowired Computer com){
+    public Alien alien(Computer com){ // @Qualifier("desktop")
         Alien obj = new Alien();
         obj.setAge(30);
         obj.setCom(com);
@@ -26,5 +29,11 @@ public class AppConfig {
 //    @Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
